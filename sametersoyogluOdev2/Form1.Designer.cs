436cdf9a1735b9423f5a32c6006bda27e5558aac
smter <smter@DESKTOP-96JC8IR>
2023-05-27 17:10:17 +0300

    Add project files.

---
+++ sametersoyogluOdev2/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace sametersoyogluOdev2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKontrol = new System.Windows.Forms.Button();
+            this.txtIfade = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnKontrol
+            // 
+            this.btnKontrol.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnKontrol.Location = new System.Drawing.Point(144, 156);
+            this.btnKontrol.Name = "btnKontrol";
+            this.btnKontrol.Size = new System.Drawing.Size(141, 40);
+            this.btnKontrol.TabIndex = 5;
+            this.btnKontrol.Text = "YIĞIN YAPISI İLE İFADEYİ KONTROL ET";
+            this.btnKontrol.UseVisualStyleBackColor = false;
+            this.btnKontrol.Click += new System.EventHandler(this.btnKontrol_Click);
+            // 
+            // txtIfade
+            // 
+            this.txtIfade.Location = new System.Drawing.Point(81, 116);
+            this.txtIfade.Name = "txtIfade";
+            this.txtIfade.Size = new System.Drawing.Size(274, 20);
+            this.txtIfade.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(78, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "İSTEDİĞİNİZ MATEMATİKSEL İFADEYİ GİRİNİZ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 304);
+            this.Controls.Add(this.btnKontrol);
+            this.Controls.Add(this.txtIfade);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKontrol;
+        private System.Windows.Forms.TextBox txtIfade;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ sametersoyogluOdev2/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sametersoyogluOdev2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKontrol_Click(object sender, EventArgs e)
+        {
+            Stack <char> stack = new Stack<char>();
+
+            char[] ilk = { '(', '{', '[' };
+            char[] son = { ')', '}', ']' };
+            bool cevap = true;
+
+            char[] oku = txtIfade.Text.ToCharArray();
+
+            if (txtIfade.Text == "")
+            {
+                MessageBox.Show("Lütfen İfade Giriniz Boş Bırakılamaz","İFADE GİRİNİZ!");
+            }
+            else
+            {
+                for (int i = 0; i < oku.Length; i++)
+                {
+                    if (ilk.Contains(oku[i]))
+                    {
+                        stack.Push(oku[i]);
+                    }
+                    else if (son.Contains(oku[i]))
+                    {
+                        try
+                        {
+                            char gecici = stack.Pop();
+                            for (int j = 0; j < ilk.Length; j++)
+                            {
+                                if (ilk[j] == gecici)
+                                {
+                                    if (son[j] != oku[i])
+                                    {
+                                        cevap = false;
+                                        MessageBox.Show("Girilen İfade Dengesizdir. Girmiş Olduğunuz Parantezler Farklıdır.");
+                                    }
+                                }
+                            }
+                        }
+                        catch
+                        {
+                            cevap = false;
+                            MessageBox.Show("Girilen İfade Gecersizdir. Başlangıç Sembolü Bulunmalıdır!");
+
+                        }
+                    }
+                }
+
+                if (stack.Count != 0)
+                {
+                    cevap = false;
+                    MessageBox.Show("Girilen İfade Gecersizdir. Parantez Kapatılmalıdır.");
+                }
+                else
+                {
+                    if (cevap == true)
+                    {
+                        MessageBox.Show("Girmiş Olduğunuz İfade Dengelidir.", "GEÇERLİ KULLANIM");
+                    }
+                }
+            }
+        }
+
+    }
+}
